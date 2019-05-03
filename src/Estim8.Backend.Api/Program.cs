1bce4fdf3e54862d15dd256c12821c4164bece3e
Lars Baunwall <lpe@thinkability.dk>
2019-05-03 12:31:19 +0200

    Rearrange solution files and folders

---
+++ src/Estim8.Backend.Api/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Estim8.Backend.Api.Registry;
+using Lamar;
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Estim8.Backend.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseLamar(new WebRegistry())
+                .UseStartup<Startup>();
+    }
+}
