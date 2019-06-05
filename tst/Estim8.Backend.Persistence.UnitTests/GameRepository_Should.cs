8ace6b99bebaa81d877d9451a6f512e60dcc0cb7
Lars Baunwall <larslb@thinkability.dk>
2019-06-05 18:00:22 +0200

    At unit test for upsetting games

--- tst/Estim8.Backend.Persistence.UnitTests/GameRepository_Should.cs
+++ tst/Estim8.Backend.Persistence.UnitTests/GameRepository_Should.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
+using Estim8.Backend.Persistence.Model;
 using Estim8.Backend.Persistence.Repositories;
 using Estim8.TestInfrastructure;
 using Moq;
@@ -35,7 +36,28 @@ namespace Estim8.Backend.Persistence.UnitTests
 
             //Assert
             answer.ShouldBe(true, "Redis should acknowledge the delete");
-            dbMock.Verify(x => x.RemoveAsync(gameId.ToString(), CommandFlags.DemandMaster), Times.Once);
+            dbMock.Verify(x => x.RemoveAsync(gameId.ToString(), CommandFlags.DemandMaster), Times.Once, "All Redis write operations must be done with DemandMaster flag");
+        }
+
+        [Theory(Skip = "Skipped until Rider/ReSharper fix for AutoData attributes is released"), AutoMoqData]
+        public async Task Create_A_Game(Game game)
+        {
+            //Arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var cacheMock = fixture.Freeze<Mock<IRedisCacheClient>>();
+            var dbMock = fixture.Freeze<Mock<IRedisDatabase>>();
+            dbMock.Setup(x => x.AddAsync(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<When>(), It.IsAny<CommandFlags>())).ReturnsAsync(true);
+            cacheMock.Setup(x => x.GetDbFromConfiguration()).Returns(dbMock.Object);
+
+            var sut = fixture.Create<GameRepository>();
+            
+            //Act
+            var answer = await sut.Upsert(game);
+
+            //Assert
+            answer.ShouldBe(true, "Redis should acknowledge the upsert");
+            dbMock.Verify(x => x.AddAsync(game.Id.ToString(), It.IsAny<object>(), It.IsAny<When>(), CommandFlags.DemandMaster), Times.Once, "All Redis write operations must be done with DemandMaster flag");
         }
     }
 }
