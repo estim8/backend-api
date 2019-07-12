2da4df757c4686362aa97af7e2a1a5fa45381daf
Lars Baunwall <lpe@thinkability.dk>
2019-07-12 12:14:12 +0200

    Separate IRepository methods into a separate base class, so not everyone needs to implement/override them

--- src/Estim8.Backend.Persistence/Repositories/GameRepository.cs
+++ src/Estim8.Backend.Persistence/Repositories/GameRepository.cs
@@ -6,7 +6,7 @@ using StackExchange.Redis.Extensions.Core.Abstractions;
 
 namespace Estim8.Backend.Persistence.Repositories
 {
-    public class GameRepository : RedisRepository<Game>
+    public class GameRepository : RedisHashRepository<Game>
     {
         public GameRepository(IContext redisContext, ILoggerFactory loggerFactory) : base(redisContext, loggerFactory)
         {
--- src/Estim8.Backend.Persistence/Repositories/RedisRepository.cs
+++ src/Estim8.Backend.Persistence/Repositories/RedisRepository.cs
@@ -12,42 +12,18 @@ using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Estim8.Backend.Persistence.Repositories
 {
-    public abstract class RedisRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    public abstract class RedisRepository<TEntity> where TEntity : Entity
     {
         protected IContext Redis;
         protected ILogger Logger;
-        protected readonly string HashSetKey;
-        public RedisRepository(IContext redisContext, ILoggerFactory loggerFactory)
+
+        protected RedisRepository(IContext redisContext, ILoggerFactory loggerFactory)
         {
             Redis = redisContext;
             Logger = loggerFactory.CreateLogger<RedisRepository<TEntity>>();
-
-            HashSetKey = $"{typeof(TEntity).Name.ToLowerInvariant()}:hash";
             
-            var redis = (RedisContext) Redis;
-            Logger.LogInformation("Redis {EntityName} repository ready. Connection is {ConnectionStatus}.", typeof(TEntity).Name, redis.GetConnectionMultiplexer().IsConnected);
-        }
-
-        protected string ToKey(Guid id, string collectionType = null)
-        {
-            return collectionType == null
-                ? $"{typeof(TEntity).Name.ToLower()}:id:{id.ToString()}"
-                : $"{collectionType}:id:{id.ToString()}";
-        }
-        
-        public virtual async Task<bool> Delete(Guid id)
-        {
-            return await Redis.Cache.RemoveHashedAsync(HashSetKey, ToKey(id));
-        }
-
-        public virtual async Task<TEntity> GetById(Guid id)
-        {
-            return await Redis.Cache.GetHashedAsync<TEntity>(HashSetKey, ToKey(id));
-        }
-
-        public virtual async Task Upsert(TEntity entity)
-        {
-            await Redis.Cache.SetHashedAsync(HashSetKey, ToKey(entity.Id), entity);
-        }
+            var redis = Redis as RedisContext;
+            Logger.LogInformation("Redis {EntityName} repository ready. Connection is {ConnectionStatus}.", typeof(TEntity).Name, redis?.GetConnectionMultiplexer().IsConnected);
+        } 
     }
 }
