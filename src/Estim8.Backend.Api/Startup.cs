12f77706ca414d0143df325fb1973570df26b13d
Lars Baunwall <larslb@thinkability.dk>
2019-07-17 19:17:09 +0200

    Expose Location header + move swagger to root

--- src/Estim8.Backend.Api/Startup.cs
+++ src/Estim8.Backend.Api/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebSockets.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -58,7 +59,8 @@ namespace Estim8.Backend.Api
                 builder
                     .AllowAnyOrigin()
                     .AllowAnyHeader()
-                    .AllowAnyMethod();
+                    .AllowAnyMethod()
+                    .WithExposedHeaders(HttpResponseHeader.Location.ToString());
             }));
         }
 
@@ -82,7 +84,11 @@ namespace Estim8.Backend.Api
             app.UseMvc();
             
             app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Estim8 API"); });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Estim8 API");
+                c.RoutePrefix = "";
+            });
         }
     }
 }
