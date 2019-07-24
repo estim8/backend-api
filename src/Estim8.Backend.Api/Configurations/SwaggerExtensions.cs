c774c3cad55364a35483ab84bf4eb475e5620502
Lars Baunwall <larslb@thinkability.dk>
2019-07-24 13:18:20 +0200

    Add auth documentation to swagger operations

--- src/Estim8.Backend.Api/Configurations/SwaggerExtensions.cs
+++ src/Estim8.Backend.Api/Configurations/SwaggerExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Filters;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Estim8.Backend.Api.Configurations
@@ -13,16 +14,7 @@ namespace Estim8.Backend.Api.Configurations
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info {Title = "Estim8 API", Version = "v1"}); 
-                c.AddSecurityDefinition("oauth2", new ApiKeyScheme()
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = "header",
-                    Type = "apiKey"
-                });
-
-                c.OperationFilter<SecurityRequirementsOperationFilter>();
-
+                
                 c.DescribeAllEnumsAsStrings();
                 c.DescribeStringEnumsInCamelCase();
                 c.DescribeAllParametersInCamelCase();
@@ -30,6 +22,17 @@ namespace Estim8.Backend.Api.Configurations
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
+                
+                c.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+                
+                c.AddSecurityDefinition("oauth2", new ApiKeyScheme()
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
             });
 
             return services;
