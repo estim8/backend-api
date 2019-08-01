5b29af2ba229d7c3d652f4ecee7a976ccb74d6ac
Lars Baunwall <larslb@thinkability.dk>
2019-08-01 16:56:50 +0200

    Remove generic constraint for RedisRepository base class

--- src/Estim8.Backend.Persistence/Repositories/IRepository.cs
+++ src/Estim8.Backend.Persistence/Repositories/IRepository.cs
@@ -5,7 +5,7 @@ using Estim8.Backend.Persistence.Model;
 
 namespace Estim8.Backend.Persistence.Repositories
 {
-    public interface IRepository<TEntity> where TEntity : Entity
+    public interface IRepository<TEntity>
     {
         Task<bool> Delete(Guid id);
         Task<TEntity> GetById(Guid id);
--- src/Estim8.Backend.Persistence/Repositories/RedisRepository.cs
+++ src/Estim8.Backend.Persistence/Repositories/RedisRepository.cs
@@ -12,8 +12,7 @@ using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Estim8.Backend.Persistence.Repositories
 {
-    public abstract class RedisRepository<TEntity> : IRepository<TEntity> 
-        where TEntity : Entity
+    public abstract class RedisRepository<T> : IRepository<T>
     {
         protected IContext Redis;
         protected ILogger Logger;
@@ -24,17 +23,17 @@ namespace Estim8.Backend.Persistence.Repositories
         {
             Redis = redisContext;
             Serializer = serializer;
-            Logger = loggerFactory.CreateLogger<RedisRepository<TEntity>>();
+            Logger = loggerFactory.CreateLogger<RedisRepository<T>>();
             Database = Redis.GetConnectionMultiplexer().GetDatabase();
 
             var redisLatency = Database.Ping(CommandFlags.DemandMaster);
-            Logger.LogInformation("Redis {EntityName} repository ready. Connection is {ConnectionStatus}. Latency is {RedisLatency}", typeof(TEntity).Name, redisLatency != TimeSpan.Zero ? "CONNECTED" : "DISCONNECTED", redisLatency);
+            Logger.LogInformation("Redis {EntityName} repository ready. Connection is {ConnectionStatus}. Latency is {RedisLatency}", typeof(T).Name, redisLatency != TimeSpan.Zero ? "CONNECTED" : "DISCONNECTED", redisLatency);
         }
 
-        protected async Task<bool> ModifyObjectInTransaction(string key, Action<TEntity> mutate)
+        protected async Task<bool> ModifyObjectInTransaction(string key, Action<T> mutate)
         {
             var tran = Database.CreateTransaction();
-            var original = await Redis.Cache.GetObjectAsync<TEntity>(key);
+            var original = await Redis.Cache.GetObjectAsync<T>(key);
             
             tran.AddCondition(Condition.StringEqual(key, Serializer.Serialize(original)));
             
@@ -50,23 +49,31 @@ namespace Estim8.Backend.Persistence.Repositories
         public string ToKey(Guid id, string collectionType = null)
         {
             return collectionType == null
-                ? $"{typeof(TEntity).Name.ToLower()}:id:{id.ToString()}"
+                ? $"{typeof(T).Name.ToLower()}:id:{id.ToString()}"
                 : $"{collectionType}:id:{id.ToString()}";
         }
         
-        async Task<bool> IRepository<TEntity>.Delete(Guid id)
+        async Task<bool> IRepository<T>.Delete(Guid id)
         {
             return await Redis.Cache.RemoveAsync(ToKey(id));
         }
 
-        async Task<TEntity> IRepository<TEntity>.GetById(Guid id)
+        async Task<T> IRepository<T>.GetById(Guid id)
         {
-            return await Redis.Cache.GetObjectAsync<TEntity>(ToKey(id));
+            return await Redis.Cache.GetObjectAsync<T>(ToKey(id));
         }
 
-        async Task IRepository<TEntity>.Upsert(TEntity entity)
+        async Task IRepository<T>.Upsert(T entity)
         {
-            await Redis.Cache.SetObjectAsync(ToKey(entity.Id), entity);
+            await Redis.Cache.SetObjectAsync(ToKey(ReadKey(entity)), entity);
+        }
+
+        private Guid ReadKey(T entity)
+        {
+            if(!(entity is Entity e))
+                throw new ArgumentException("T is not of type Entity", nameof(entity));
+
+            return e.Id;
         }
     }
 }
