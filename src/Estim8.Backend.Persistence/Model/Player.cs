75871fcf3575e2675667facac5d51dee0a356b63
Lars Baunwall <larslb@thinkability.dk>
2019-08-10 23:47:36 +0200

    Add ProtoContract, so that players are properly serialized in Redis

--- src/Estim8.Backend.Persistence/Model/Player.cs
+++ src/Estim8.Backend.Persistence/Model/Player.cs
@@ -1,8 +1,11 @@
+using ProtoBuf;
+
 namespace Estim8.Backend.Persistence.Model
 {
+    [ProtoContract]
     public class Player : Entity
     {
-        public string Email { get; set; }
-        public string PlayerName { get; set; }
+        [ProtoMember(1)] public string Email { get; set; }
+        [ProtoMember(2)] public string PlayerName { get; set; }
     }
 }
