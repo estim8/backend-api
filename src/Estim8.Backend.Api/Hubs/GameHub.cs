2ab06247b7556770b41be91ece4eb677f6b047c0
Lars Baunwall <larslb@thinkability.dk>
2019-08-11 00:25:56 +0200

    Notify other signalr clients of disconnected players

--- src/Estim8.Backend.Api/Hubs/GameHub.cs
+++ src/Estim8.Backend.Api/Hubs/GameHub.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Estim8.Backend.Api.Hubs.Messages;
+using Estim8.Backend.Persistence.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -9,11 +11,32 @@ namespace Estim8.Backend.Api.Hubs
     [Authorize]
     public class GameHub : Hub<IPlayerClient>
     {
-        public override Task OnConnectedAsync()
+        private readonly IPlayerRepository _playerRepo;
+
+        public GameHub(IPlayerRepository playerRepo)
+        {
+            _playerRepo = playerRepo;
+        }
+        public override async Task OnConnectedAsync()
         {
             var gameId = Context.User.FindFirstValue("game_id");
-            Groups.AddToGroupAsync(Context.ConnectionId, gameId);
-            return base.OnConnectedAsync();
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+            await base.OnConnectedAsync();
         }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            if (Guid.TryParse(Context.User.FindFirstValue("game_id"), out var gameId) &&
+                Guid.TryParse(Context.User.FindFirstValue("player_id"), out var playerId))
+            {
+                await _playerRepo.DeletePlayer(gameId, playerId);
+                
+                await Clients.OthersInGroup(gameId.ToString())
+                    .PlayerRemovedFromGame(new PlayerMessage {GameId = gameId, PlayerId = playerId});
+            }
+            
+            await base.OnDisconnectedAsync(exception);
+        }
+        
     }
 }
